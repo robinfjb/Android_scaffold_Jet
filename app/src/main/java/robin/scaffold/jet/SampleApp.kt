ada79820f4c100c843866e3e3be7bd53009d67ba
robin.fang <robin.fang@wayz.ai>
2020-06-30 16:06:32 +0800

    add application

---
+++ app/src/main/java/robin/scaffold/jet/SampleApp.kt
@@ -0,0 +1,33 @@
+package robin.scaffold.jet
+
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+
+class SampleApp : Application(), LifecycleObserver {
+    companion object {
+        private lateinit var instance: SampleApp
+        var isAppInForeground = false
+        fun getAppContext(): Context =
+                instance.applicationContext
+    }
+
+
+    /**
+     * Callback when the app is open but backgrounded
+     */
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    fun onAppBackgrounded() {
+        isAppInForeground = false
+    }
+
+    /**
+     * Callback when the app is foregrounded
+     */
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun onAppForegrounded() {
+        isAppInForeground = true
+    }
+}
